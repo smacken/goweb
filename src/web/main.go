842946a975375191ff8ab4fb9c66c88be8474a64
Scott <scott.mac.web@gmail.com>
2014-10-14 19:19:04 +1000

    static path prefix

--- src/web/main.go
+++ src/web/main.go
@@ -11,11 +11,9 @@ func main() {
 	router := mux.NewRouter()
 	homeController := new(controllers.HomeController)
 	router.HandleFunc("/", homeController.Index)
-	router.Handle("/views", http.FileServer(http.Dir("../web/views")))
-	/*router.HandleFunc("/views/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, r.URL.Path[1:])
-	})*/
-	//http.HandleFunc("/", homeController.Index)
+	api := router.PathPrefix("/api/").Subrouter()
+	router.PathPrefix("/views/")
+		  .Handler(http.StripPrefix("/views/", http.FileServer(http.Dir("views/"))))
 
 	http.Handle("/", router)
 	log.Println("Listening...")
